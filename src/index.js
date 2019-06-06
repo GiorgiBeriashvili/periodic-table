9e4bcc6a368dd462009955b88b7c538cbb398d30
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2019-06-07 03:33:08 +0400

    Add .babelrc and .eslint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import { join } from "path";
 
 const application = express();
 const port = 3000;
 
-application.use(express.static(path.join(__dirname, "/../dist/static")));
+application.use(express.static(join(__dirname, "/../dist/static")));
 
 application.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, "/../dist/views", "index.html"));
+    response.sendFile(join(__dirname, "/../dist/views", "index.html"));
 });
 
 application.get("*", (request, response) => {
     response.send(`The route "${request.url}" led you here.`);
 });
 
-application.listen(port, () => {
-    console.log(`Example application listening on port ${port}!`);
-});
+application.listen(port);
