9d50779ffdb720b6b364d87ec36970f08b3bf39a
Giorgi Beriashvili <giorgi.beriashvili@outlook.com>
2019-06-07 02:57:38 +0400

    Update index.js
    Rename variables to their full names.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
 
-const app = express()
-const port = 3000
+const application = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, "/../dist/static")));
+application.use(express.static(path.join(__dirname, "/../dist/static")));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/../dist/views", "index.html"));
+application.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "/../dist/views", "index.html"));
 });
 
-app.get("*", (req, res) => {
-    res.send(`The route "${req.url}" led you here.`);
+application.get("*", (request, response) => {
+    response.send(`The route "${request.url}" led you here.`);
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+application.listen(port, () => {
+    console.log(`Example application listening on port ${port}!`);
 });
